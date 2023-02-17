1f8d48284485fc3de22feb14b83be0b034ae9173
Paracetamole1 <102689636+Paracetamole1@users.noreply.github.com>
2023-02-17 09:45:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,38 +11,38 @@ def main():
 
     for script in scripts:
         if "window.__PRELOADED_STATE_LISTING__" in script.text:
-            bd = script.text.replace('window.__PRELOADED_STATE_LISTING__ = ',"").split(';if(')[0]
+            db = script.text.replace('window.__PRELOADED_STATE_LISTING__ = ',"").split(';if(')[0]
             break
         
-    data = json.loads(bd)
+    data = json.loads(db)
 
     array_df = []
 
     for i in range (len(data['search']['aggs']['vehicle.make'])):
                     
-        marques = data['search']['aggs']['vehicle.make'][i]['key']
-        if marques.lower()==brand_entry:
+        brands_name = data['search']['aggs']['vehicle.make'][i]['key']
+        if brands_name.lower()==brand_entry:
             
             for e in range(len(data['search']['aggs']['vehicle.make'][i]['agg'])):
                 
                 for page in range(1,5):
-                    car_modele = data['search']['aggs']['vehicle.make'][i]['agg'][e]['key']
-                    url = "https://www.lacentrale.fr/listing?makesModelsCommercialNames="+marques+":"+car_modele+"&page="+str(page)
+                    car_model = data['search']['aggs']['vehicle.make'][i]['agg'][e]['key']
+                    url = "https://www.lacentrale.fr/listing?makesModelsCommercialNames="+brands_name+":"+car_model+"&page="+str(page)
                     small_soup = BeautifulSoup(requests.get(url).text, 'html.parser')
                     scripts = small_soup.find_all("script")
                     
                     for script in scripts:
                         if "window.__PRELOADED_STATE_LISTING__" in script.text:
-                            bd = script.text.replace('window.__PRELOADED_STATE_LISTING__ = ',"").split(';if(')[0]
+                            db = script.text.replace('window.__PRELOADED_STATE_LISTING__ = ',"").split(';if(')[0]
                             break
-                    data = json.loads(bd)
+                    data = json.loads(db)
                     if len(data['search']['hits']) == 0:
                         break
                     
                     for car in data['search']['hits']:
                         car = car['item']
                         array_df.append([
-                                        marques,
+                                        brands_name,
                                         car['vehicle']['model'],
                                         car['vehicle'].get('motorization') or car['vehicle'].get('version'),
                                         car['vehicle']['year'],
